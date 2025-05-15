0fe9be82de711b3fd99853a7000c09feef466885
mood7dev <mood7dev@gmail.com>
2025-05-15 18:09:33 +0900

    잔디

--- src/ch04/sec03/SwitchStrinf.java
+++ src/ch04/sec03/SwitchStrinf.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class SwitchStrinf {
     public static void main(String[] args) {
-String gender = "남";
-            switch (gender) {
-                case "w", "W", "여자", "여":
-                    System.out.println("여자");
-                    break;
-                case "m", "M", "남자", "남":
-                    System.out.println("남자");
-                    break;
-                default:
-                    System.out.println("다시 입력해주세요");
-                    break;
-            }
+        String gender = "남";
+        switch (gender) {
+            case "w", "W", "여자", "여":
+                System.out.println("여자");
+                break;
+            case "m", "M", "남자", "남":
+                System.out.println("남자");
+                break;
+            default:
+                System.out.println("다시 입력해주세요");
+                break;
         }
     }
+}
 
 
